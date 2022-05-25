0751beffb57de8f0bca76b1d1c53147a76edb187
Seoning <103872311+Seoning@users.noreply.github.com>
2022-05-25 17:37:23 +0900

    Add files via upload
    책이름, 작가, 가격

---
+++ 연습.java
@@ -0,0 +1,37 @@
+class Novel{
+	static String author;
+	int price;
+	String bookname;
+	
+	void setAuthor(String author) {
+		Novel.author = author;
+	}
+	void setPrice(int price) {
+		this.price = price;
+	}
+	void setBookname(String bookname) {
+		this.bookname = bookname;
+	}
+	String getAuthor() {
+		return author;
+	}
+	int getPrice() {
+		return price;
+	}
+	String getBookname() {
+		return bookname;
+	}
+}
+
+//자동으로 getter & setter 만들기 = alt + shift + s.
+public class 연습 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+	}
+
+}
